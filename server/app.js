07922639000cac65f66e4fef1d9b39b1c9a2196f
Danyal Arman <danyalarman15@gmail.com>
2025-06-13 22:12:31 +0530

    centralized api base url

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ ConnectDB()
 
 // Allow frontend (5173) to access backend (3000)
 app.use(cors({
-    origin: "https://eduglide-client.onrender.com" || "http://localhost:5173", 
+    origin: "https://eduglide-client.onrender.com", 
     credentials:true 
 }))
 
