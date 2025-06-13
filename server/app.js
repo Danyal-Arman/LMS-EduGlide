371e595634578f856f190ee9205589dbea748609
Danyal Arman <danyalarman15@gmail.com>
2025-06-13 22:36:01 +0530

    changes in env and use dynamic port

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use('/lecture', lectureRoute);
 app.use('/media', mediaRoute) 
 app.use('/purchase', paymentRoute)
 app.use('/course-progress', courseProgressRoute)
-const port =  3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
